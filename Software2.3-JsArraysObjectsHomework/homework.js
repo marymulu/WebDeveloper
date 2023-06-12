6ab6059c6d14e35a66355a04212612e4b6c6f27a
marymulu <133064728+marymulu@users.noreply.github.com>
2023-06-11 23:33:29 -0400

    changes to 2.3 folder

--- Software2.3-JsArraysObjectsHomework/homework.js
+++ Software2.3-JsArraysObjectsHomework/homework.js
@@ -1,22 +1,25 @@
 // *Q1: 
 // 1. Create an array called grades and put the following values: 56, 87, 99, 40
 
-
+let grades = [56,87,99,40]
+console.log (grades)
 
 
 // 2. Add another element at the end of the array with value: 68
 
-
+grades.push(68)
+console.log(grades)
 
 
 // 3. Remove the 1st element of the array
 
 
-
+grades.splice(0,1);
+console.log(grades)
 
 // 4. Change 40 to 60.
-
-
+grades [3] = 60
+console.log(grades)
 
 
 // 5. Create a variable called average and compute for the average of the grades array.
